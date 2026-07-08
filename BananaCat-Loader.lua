1d34dd34d6458d97ba3d0cf04bad89db04c3eccb
doraimon0 <86925746+doraimon0@users.noreply.github.com>
2026-07-09 00:35:22 +0300

    Add files via upload

---
+++ BananaCat-Loader.lua
@@ -0,0 +1,2 @@
+local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
+loadstring(HttpGet('https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua'))()
