ac6e5963c6d3e88f7bac698b45a78a18950c10a5
doraimon0 <86925746+doraimon0@users.noreply.github.com>
2026-07-22 05:59:33 +0300

    Delete BananaCat-Loader.lua

--- BananaCat-Loader.lua
+++
@@ -1,2 +0,0 @@
-local HttpGet = loadstring(game:HttpGet('https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/request.lua'))().HttpGet
-loadstring(HttpGet('https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua'))()
