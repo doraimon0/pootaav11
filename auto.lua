2e014ca00fdf91376ddc13341191b3446ef75d85
doraimon0 <86925746+doraimon0@users.noreply.github.com>
2026-07-29 22:33:59 +0300

    Delete auto.lua

--- auto.lua
+++
@@ -1,9 +0,0 @@
-getgenv().mode = "noui"
-getgenv().fps_limit = 30
--- Enter the share code shown on your main account.
-getgenv().share_code = "g264879666"
--- Keep it enabled to continue to get lastest sync data from your main account.
-getgenv().share_autosync = true
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Reversed1337/Testing-123/refs/heads/main/zeta%20hub.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ScriptHA/Scripts/refs/heads/main/FPS%20Boost.luau"))()
